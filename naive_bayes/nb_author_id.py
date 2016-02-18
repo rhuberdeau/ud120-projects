a799eab460f388d13a6bdfebb3fc57edc10face8
rhuberdeau <rhuberdeau@gmail.com>
2016-02-17 20:46:18 -0500

    code to find the author of each email

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -14,13 +14,24 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.naive_bayes import GaussianNB
 
 
 ### features_train and features_test are the features for the training
 ### and testing datasets, respectively
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
+clf = GaussianNB()
 
+t0 = time()
+clf.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+t0 = time()
+clf.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+print(clf.score(features_train, labels_train))
 
 
 
